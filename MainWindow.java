30f2ce609c152d78da47d10b15d173a93b5f702d
Phanta <grunitz@KVL-PC.(none)>
2012-05-18 14:27:08 +0200

    First Version
    some GUI basics implemented

---
+++ MainWindow.java
@@ -0,0 +1,50 @@
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;	// screenresolution
+import java.awt.Dimension;	// dito
+
+
+public class MainWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private static Dimension dimWindow = new Dimension(1024, 768);
+	private static String strTitle = "Signal Display Tool";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new MainWindow();
+	}
+
+	MainWindow() {
+		//native look and feel
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("Couldn't set native Look and Feel.");
+		}
+		
+		// put window in the middle of screen with default size
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension dimScreenResolution = tk.getScreenSize();
+		int x = (dimScreenResolution.width - dimWindow.width) / 2;
+		int y = (dimScreenResolution.height - dimWindow.height) / 2;
+		this.setBounds(x, y, dimWindow.width, dimWindow.height);
+
+		// define default behavior
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle(strTitle);
+		
+		// add Menus
+		this.setJMenuBar(Menus.getInstance());
+		
+		// add Toolbar
+		this.add(Toolbar.getInstance(), BorderLayout.PAGE_START);
+		
+		// show me what u got!
+		setVisible(true);
+	}
+}
---
+++ Menus.java
@@ -0,0 +1,81 @@
+/* Class for the (Main-) Menus of the Program.
+ * 
+ * contains all the menus including:
+ * 		naming of the items
+ * 		accelerators
+ * 		function calls
+ */
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+
+public class Menus extends JMenuBar {
+	
+	private static final long serialVersionUID = 1L;
+	private static Menus myself = new Menus();
+	
+	public static JMenuBar getInstance() {
+		return myself;
+	}
+	
+	protected Menus() {
+		JMenu m;
+		JMenuItem mi;
+		
+		// --- DATEI ----------------------------------------------------------
+		m = new JMenu("Datei");
+		m.setMnemonic(KeyEvent.VK_D);
+		mi = new JMenuItem("Signal laden...",KeyEvent.VK_S);
+		m.add(mi);
+		mi = new JMenuItem("Annotationen laden...",KeyEvent.VK_A);
+		m.add(mi);
+		m.addSeparator();
+		mi = new JMenuItem("Beenden",KeyEvent.VK_B);
+		mi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.CTRL_DOWN_MASK));
+		// quick'n'dirty test of accelerator and actionlistener
+			ActionListener al = new ActionListener() {
+				public void actionPerformed( ActionEvent e ) {
+					System.exit( 0 );
+				}
+			};
+			mi.addActionListener(al);		
+		m.add(mi);
+		this.add(m);
+		
+		// --- BEARBEITEN ----------------------------------------------------------
+		m = new JMenu("Bearbeiten");
+		m.setMnemonic(KeyEvent.VK_B);
+		mi = new JMenuItem("Annotation kopieren");
+		mi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+		m.add(mi);
+		mi = new JMenuItem("Annotation einfügen");
+		mi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+		m.add(mi);
+		m.addSeparator();
+		mi = new JMenuItem("Einstellungen...", KeyEvent.VK_E);
+		m.add(mi);
+		this.add(m);
+		
+		// --- ANSICHT ----------------------------------------------------------
+		m = new JMenu("Ansicht");
+		m.setMnemonic(KeyEvent.VK_A);
+		this.add(m);
+		
+		// --- HILFE ----------------------------------------------------------
+		m = new JMenu("Hilfe");
+		m.setMnemonic(KeyEvent.VK_H);
+		mi = new JMenuItem("Test 1");
+		m.add(mi);
+		mi = new JMenuItem("Test 2");
+		m.add(mi);
+		
+		this.add(m);
+	}
+}
